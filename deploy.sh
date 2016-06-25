ff309221903c19d173d4e201b21918bda4febf36
Simon Robb <sajrobb@gmail.com>
2016-06-25 18:57:54 +1000

    Updated deploy script to use AWS ECS

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,105 @@
-#! /bin/bash
-# deploy.sh
+#!/usr/bin/env bash
 
-IMAGE_NAME=$1
-SHA1=$2
-EB_ENVIRONMENT=$3
+set -e
+set -u
+set -o pipefail
 
-# Deploy image to Docker Hub
-docker push $IMAGE_NAME:$SHA1
+# more bash-friendly output for jq
+JQ="jq --raw-output --exit-status"
 
-# Create a new Elastic Beanstalk version
-EB_BUCKET=calcium-deployment
-EB_APPLICATION=Calcium
-DOCKER_ZIP=$SHA1-Docker.zip
+deploy_image() {
 
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > Dockerrun.aws.json
+    eval "$(aws ecr get-login --region us-east-1)"
+    docker tag $IMAGE_NAME:$CIRCLE_SHA1 674110226179.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_NAME:$CIRCLE_SHA1
+    docker push 674110226179.dkr.ecr.us-east-1.amazonaws.com/$IMAGE_NAME:$CIRCLE_SHA1
+}
 
-# Create archive to ship
-zip $CIRCLE_ARTIFACTS/$DOCKER_ZIP -r Dockerrun.aws.json .ebextensions/
+# reads $CIRCLE_SHA1, $host_port
+# sets $task_def
+make_task_def() {
 
-# Create new Elastic Beanstalk version
-aws s3 cp .dockercfg s3://$EB_BUCKET/dockercfg
-aws s3 cp $CIRCLE_ARTIFACTS/$DOCKER_ZIP s3://$EB_BUCKET/$DOCKER_ZIP
-aws elasticbeanstalk create-application-version --application-name $EB_APPLICATION \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKER_ZIP
+    task_template='[
+      {
+        "volumesFrom": [],
+        "memory": 300,
+        "extraHosts": null,
+        "dnsServers": null,
+        "disableNetworking": null,
+        "dnsSearchDomains": null,
+        "portMappings": [
+          {
+            "hostPort": $host_port,
+            "containerPort": 8081,
+            "protocol": "tcp"
+          }
+        ],
+        "hostname": null,
+        "essential": true,
+        "entryPoint": [],
+        "mountPoints": [],
+        "name": "calcium-staging",
+        "ulimits": null,
+        "dockerSecurityOptions": null,
+        "environment": [],
+        "links": [],
+        "workingDirectory": null,
+        "readonlyRootFilesystem": null,
+        "image": "674110226179.dkr.ecr.us-east-1.amazonaws.com/calcium/website:%s",
+        "command": [],
+        "user": null,
+        "dockerLabels": null,
+        "logConfiguration": null,
+        "cpu": 10,
+        "privileged": null
+      }
+    ]'
 
-# Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name $EB_ENVIRONMENT \
-    --version-label $SHA1
+    task_def=$(printf "$task_template" $CIRCLE_SHA1 $host_port)
+
+}
+
+# reads $family
+# sets $revision
+register_definition() {
+
+    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+        echo "Revision: $revision"
+    else
+        echo "Failed to register task definition"
+        return 1
+    fi
+
+}
+
+deploy_cluster() {
+
+    host_port=80
+    family="Calcium"
+
+    make_task_def
+    register_definition
+    if [[ $(aws ecs update-service --cluster calcium-staging --service calcium-staging-service --task-definition $revision | \
+                   $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+    fi
+
+    # wait for older revisions to disappear
+    # not really necessary, but nice for demos
+    for attempt in {1..30}; do
+        if stale=$(aws ecs describe-services --cluster calcium-staging --services calcium-staging-service | \
+                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
+            echo "Waiting for stale deployments:"
+            echo "$stale"
+            sleep 5
+        else
+            echo "Deployed!"
+            return 0
+        fi
+    done
+    echo "Service update took too long."
+    return 1
+}
+
+deploy_image
+deploy_cluster
