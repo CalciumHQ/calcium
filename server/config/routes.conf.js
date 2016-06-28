83da7d295dd0f7c3fa263a076b9ef65ff5ea88b1
Simon Robb <sajrobb@gmail.com>
2016-06-28 22:35:21 +1000

    Built server

--- server/config/routes.conf.js
+++ server/config/routes.conf.js
@@ -7,7 +7,7 @@ var RoutesConfig = (function () {
     function RoutesConfig() {
     }
     RoutesConfig.init = function (application) {
-        var _clientFiles = (process.env.NODE_ENV === 'production') ? '/client/dist/' : '/client/.tmp/';
+        var _clientFiles = (process.env.NODE_ENV === 'development') ? '/client/.tmp/' : '/client/dist/';
         var _root = process.cwd();
         application.use(express.static(_root));
         application.use(express.static(_root + _clientFiles));
